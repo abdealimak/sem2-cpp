cf2b1604a28266d2f5e0f5d4e28df2ebef5c5697
Abdeali Makda <abdealimac@gmail.com>
2026-01-07 23:05:38 +0530

    Add files via upload

---
+++ size.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char d;
+    bool e;
+
+    cout << sizeof(a) << " bytes" << endl;
+    cout << sizeof(b) << " bytes" << endl;
+    cout << sizeof(c) << " bytes" << endl;
+    cout << sizeof(d) << " bytes" << endl;
+    cout << sizeof(e) << " bytes" << endl;
+
+    return 0;
+}
