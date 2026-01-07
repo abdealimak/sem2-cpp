bf3773d2b7c5fc84c0f0cea154aaf276db3dcdc2
Abdeali Makda <abdealimac@gmail.com>
2026-01-07 23:15:14 +0530

    Add files via upload

---
+++ joiningstrings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s1 = "hello";
+    string s2 = "world";
+
+    //concatenation:
+
+    string s3 = s1 + s2;
+    cout<<s3;
+
+    //for appending
+    //you dont need an extra variable
+    //code:
+    //s1 += /s2
+
+}
