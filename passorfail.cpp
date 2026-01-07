f8d4112daa45120af87ce8352a1f8f42521bccdf
Abdeali Makda <abdealimac@gmail.com>
2026-01-07 23:06:37 +0530

    Add files via upload

---
+++ passorfail.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout<<"enter marks: ";
+    cin>>a;
+
+    (a >= 40) ? cout<< "Pass" : cout<< "Fail";
+    
+}
---
+++ printgreater.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cout<<"enter first number: ";
+    cin>>a;
+
+    cout<<"enter sec number: ";
+    cin>>b;
+
+    if (a>b) {
+    cout<<a;
+}
+else {
+    cout<<b;
+}
+//int max = (a > b) ? a : b;
+//cout<<"largest number is: "<<max;
+}
