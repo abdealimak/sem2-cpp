b5659d2876d51c7adfcb041e12543be360f834b2
Abdeali Makda <abdealimac@gmail.com>
2026-01-28 21:21:32 +0530

    Add files via upload

---
+++ pattern problems/secnd.cpp
@@ -0,0 +1,19 @@
+/*
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 6; i++){
+        for(int j = 1; j <= i; j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
