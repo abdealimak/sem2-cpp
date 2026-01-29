fa25830b596e05ce67d00dba5b15dae0bc8ac414
Abdeali Makda <abdealimac@gmail.com>
2026-01-29 22:01:51 +0530

    Update and rename 2darray.cpp to arrays/2darray.cpp

--- arrays/2darray.cpp
+++ arrays/2darray.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main(){
     int a[2][3];
 
-    cout<<"enter elements of 2d array: \n";
+    cout << "enter elements of 2d array:\n";
     for(int i = 0; i < 2; i++){
         for(int j = 0; j < 3; j++){
             cin >> a[i][j];
         }
     }
 
-    cout<<"array elements are: \n";
-    for(int i = 0; i < 2; i ++){
+    cout << "array elements are:\n";
+    for(int i = 0; i < 2; i++){
         for(int j = 0; j < 3; j++){
-            cout<<a[i][j];
+            cout << a[i][j] << " ";
         }
+        cout << endl;
     }
     return 0;
 }
