2b5a5daedcbfc7501ecb80f7f1460e8669d7eacc
Abdeali Makda <abdealimac@gmail.com>
2026-01-29 22:00:43 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[2][3];
+
+    cout<<"enter elements of 2d array: \n";
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    cout<<"array elements are: \n";
+    for(int i = 0; i < 2; i ++){
+        for(int j = 0; j < 3; j++){
+            cout<<a[i][j];
+        }
+    }
+    return 0;
+}
